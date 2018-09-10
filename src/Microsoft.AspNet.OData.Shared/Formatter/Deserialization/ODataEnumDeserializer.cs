ed768495930de9827358ad8053c56b740fd4c785
Thomas Louis <thomas.louis@credensis.eu>
2018-09-10 23:26:58 +0200

    Enum deserialization for untyped entities corrected.

--- src/Microsoft.AspNet.OData.Shared/Formatter/Deserialization/ODataEnumDeserializer.cs
+++ src/Microsoft.AspNet.OData.Shared/Formatter/Deserialization/ODataEnumDeserializer.cs
@@ -7,66 +7,56 @@ using Microsoft.AspNet.OData.Common;
 using Microsoft.OData;
 using Microsoft.OData.Edm;
 
-namespace Microsoft.AspNet.OData.Formatter.Deserialization
-{
+namespace Microsoft.AspNet.OData.Formatter.Deserialization {
     /// <summary>
     /// Represents an <see cref="ODataDeserializer"/> that can read OData enum types.
     /// </summary>
-    public class ODataEnumDeserializer : ODataEdmTypeDeserializer
-    {
+    public class ODataEnumDeserializer : ODataEdmTypeDeserializer {
         /// <summary>
         /// Initializes a new instance of the <see cref="ODataEnumDeserializer"/> class.
         /// </summary>
         public ODataEnumDeserializer()
-            : base(ODataPayloadKind.Property)
-        {
+            : base( ODataPayloadKind.Property ) {
         }
 
         /// <inheritdoc />
-        public override object Read(ODataMessageReader messageReader, Type type, ODataDeserializerContext readContext)
-        {
-            if (messageReader == null)
-            {
-                throw Error.ArgumentNull("messageReader");
+        public override object Read( ODataMessageReader messageReader, Type type, ODataDeserializerContext readContext ) {
+            if ( messageReader == null ) {
+                throw Error.ArgumentNull( "messageReader" );
             }
-            if (type == null)
-            {
-                throw Error.ArgumentNull("type");
+            if ( type == null ) {
+                throw Error.ArgumentNull( "type" );
             }
-            if (readContext == null)
-            {
-                throw Error.ArgumentNull("readContext");
+            if ( readContext == null ) {
+                throw Error.ArgumentNull( "readContext" );
             }
 
             IEdmTypeReference edmType = readContext.GetEdmType(type);
-            Contract.Assert(edmType != null);
+            Contract.Assert( edmType != null );
 
             ODataProperty property = messageReader.ReadProperty();
-            return ReadInline(property, edmType, readContext);
+            return ReadInline( property, edmType, readContext );
         }
 
         /// <inheritdoc />
-        public override object ReadInline(object item, IEdmTypeReference edmType, ODataDeserializerContext readContext)
-        {
-            if (item == null)
-            {
+        public override object ReadInline( object item, IEdmTypeReference edmType, ODataDeserializerContext readContext ) {
+            if ( item == null ) {
                 return null;
             }
 
             ODataProperty property = item as ODataProperty;
-            if (property != null)
-            {
+            if ( property != null ) {
                 item = property.Value;
             }
 
-            if (readContext.IsUntyped)
-            {
-                Contract.Assert(edmType.TypeKind() == EdmTypeKind.Enum);
-                return new EdmEnumObject((IEdmEnumTypeReference)edmType, ((ODataEnumValue)item).Value);
+            if ( readContext.IsUntyped ) {
+                Contract.Assert( edmType.TypeKind() == EdmTypeKind.Enum );
+                return new EdmEnumObject( edmType.AsEnum(), ( (ODataEnumValue)item ).Value );
+                // return new EdmEnumObject((IEdmEnumTypeReference)edmType, ((ODataEnumValue)item).Value);
             }
 
             Type clrType = EdmLibHelpers.GetClrType(edmType, readContext.Model);
-            return EnumDeserializationHelpers.ConvertEnumValue(item, clrType);
+            return EnumDeserializationHelpers.ConvertEnumValue( item, clrType );
         }
     }
 }
